2c0165350817a5f483ed7cf00b88d38f854cd440
Mariusz Białas <mariuszbialas@macbook-pro-mb.speedport.ip>
2023-07-11 11:42:29 +0200

    rest-api works

--- app.js
+++ app.js
@@ -1,42 +1,26 @@
-'use strict';
-// require('dotenv').config();
+// 'use strict';
 
-import express from 'express';
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 
-// couchDB CONNECTION
-import nano from 'nano';
-const db = nano('http://admin:alfa@127.0.0.1:5984').db;
-const dbName = 'calendar';
-
-import apiRouter from './routes/api.js';
-
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname)));
 
 // ROUTES
-app.get('/api', (req, res) => {
-    db.list()
-      .then((res) => {
-        if (!res.includes(dbName)) {
-          db.create(dbName);
-          console.log('Databas created successfully');
-        } else {
-          console.log('Databse connecion');
-        }
-      })
-      .catch(console.warn);
-  
-      // db.get('calendar').then(data=> res.json(data));
-  
-      db.use(dbName)
-          .list({include_docs: true})
-          .then(res => res.rows.map(row => row.doc))
-          .then(data => res.json(data));
-      
-  });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+// import Routes
+const apiRouter = require('./routes/api.js'); 
+app.use('/api', apiRouter);
 
 
-const init = () => {
+// INIT
+  const init = () => {
   app.listen(3000, (err) =>
     console.log(err || 'Server läuft: http://localhost:3000')
   );
--- routes/api.js
+++ routes/api.js
@@ -1,26 +1,32 @@
-import express  from 'express';
-const apiRouter = express();
+require('dotenv').config();
 
+const express = require('express');
+const router = express.Router();
 
-apiRouter.get('/api', (req, res) => {
-    db.list()
-      .then((res) => {
-        if (!res.includes(dbName)) {
-          db.create(dbName);
-          console.log('Databas created successfully');
-        } else {
-          console.log('Databse connecion');
-        }
-      })
-      .catch(console.warn);
-  
-      // db.get('calendar').then(data=> res.json(data));
-  
-      db.use(dbName)
-          .list({include_docs: true})
-          .then(res => res.rows.map(row => row.doc))
-          .then(data => res.json(data));
-      
-  });
+// couchDB CONNECTION
+const db = require('nano')(process.env.URL).db;
+const dbName = 'calendar';
+const dbID = '4c379d1cb2ae152f0e7d17472c000f51';
 
- export default apiRouter;
+router.get('/', (req, res) => {
+
+  db.use(dbName)
+    .get(dbID, { include_docs: true })
+    .then(data => res.status(200).json(data))
+    .catch(console.warn);
+    
+});
+ 
+router.get('/:query', (req, res) => {
+  db.use(dbName)
+    .get(dbID, { include_docs: true })
+    .then(doc => {
+      const data = doc[req.params.query];
+      console.log(data);
+      if (data) res.status(200).json(data);
+      else res.status(404).send({ message: 'ungültige Daten' });
+    })
+    .catch(console.warn);
+});
+
+module.exports = router;
